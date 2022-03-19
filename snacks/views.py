38d50e9db24b189cdd73f002dfb6828a8482a08e
Fadab2 <ali_berg1@yahoo.com>
2022-03-19 16:58:37 -0700

    lab done

--- snacks/views.py
+++ snacks/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
 from .serializer import SnackSerializer
 from .models import Snack
+from .permissions import IsOwnerOrReadOnly
 
 class SnackList(generics.ListCreateAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = Snack.objects.all()
     serializer_class = SnackSerializer
 
 class SnackDetail(generics.RetrieveUpdateAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = Snack.objects.all()
     serializer_class = SnackSerializer
